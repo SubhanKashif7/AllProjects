85f234f41b1bc354225194f996828f812eb7a9a2
= <=>
2024-03-25 10:21:59 -0400

    Setup project Github Id

--- web_projects/frontend_projects/frameworks/React/Github-Id/src/App.jsx
+++ web_projects/frontend_projects/frameworks/React/Github-Id/src/App.jsx
@@ -1,33 +1,62 @@
+import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+
+  const [searchInputText , setSearchInputText] = useState('');
+  const [avatarAddress , setAvatarAddress] = useState('');
+  const [followers , setFollowers] = useState(0);
+  const [loginName , setLoginName] = useState('');
+  const [name , setName] = useState('');
+  const [repo , setRepo] = useState(0);
+  const [following , setFollowing] = useState(0);
+  const [isHirable , setIsHirable] = useState(false);
+  const [bio , setBio] = useState('');
+  const [display , setDisplay] = useState("none");
+  
+
+  useEffect(()=>{
+    try {
+      fetch(`https://api.github.com/users/${searchInputText}`).then((response)=>{
+        return response.json();
+      }).then((response)=>{
+        setAvatarAddress(response.avatar_url);
+        setFollowers(response.followers);
+        setLoginName(response.login);
+        setName(response.name);
+        setRepo(response.public_repos);
+        setIsHirable(response.hireable);
+        setFollowing(response.following);
+        setBio(response.bio);
+      });
+    } catch (error) {
+      
+    }
+  },[searchInputText])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full h-screen flex justify-center items-center bg-[#0D1117]'>
+        <div className='p-2 rounded-sm flex justify-center items-center bg-white flex-col gap-3'>
+            <input type="text" placeholder='Search User...'  onChange={(e)=>{
+              setSearchInputText(e.target.value);
+            }} className='p-2 border-2'/>
+            <div>
+              <img src={avatarAddress} alt="userImg" className='rounded-sm w-[400px]'/>
+            </div>
+            <div className='w-full'>
+              <button className='bg-black text-white w-full py-3' onClick={()=>{
+                setDisplay("flex");
+              }}>Next</button>
+            </div>
+
+
+
+        </div>
+      
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
