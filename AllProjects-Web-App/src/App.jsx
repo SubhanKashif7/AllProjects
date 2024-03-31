6750c380c4ddad81c50f8da94a3cbc82d2a782f8
= <=>
2024-03-31 14:31:23 -0700

    SETUP MAIN ALLPROJECTS WEB APP

--- AllProjects-Web-App/src/App.jsx
+++ AllProjects-Web-App/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Button from "@mui/material/button"
+import {Outlet} from "react-router-dom"
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
-    <div>App</div>
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
   )
 }
 
--- AllProjects-Web-App/src/main.jsx
+++ AllProjects-Web-App/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <App />,
+    children : [
+      {
+        path : "/",
+        element : <Home />
+      },
+      {
+        path : "/about",
+        element : <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
