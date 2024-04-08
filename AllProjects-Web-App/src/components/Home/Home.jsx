30a1894b7a626a71ef4a8a177f52837196521b5f
? <=>
2024-04-08 23:06:22 +0500

    Started Beta Home page

--- AllProjects-Web-App/src/components/Home/Home.jsx
+++ AllProjects-Web-App/src/components/Home/Home.jsx
@@ -1,91 +1,26 @@
-'use client'
-
 import React from 'react'
-import { Menu, X, ChevronDown, ChevronRight } from 'lucide-react'
-
-const menuItems = [
-  {
-    name: 'Home',
-    href: '#',
-  },
-  {
-    name: 'About',
-    href: '#',
-  },
-  {
-    name: 'Contact',
-    href: '#',
-  },
-]
-
-export default function Home() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
+import Button from "@mui/material"
+function Home() {
   return (
-    <div className="relative w-full">
-    
-      <div className="relative isolate z-0 bg-white px-6 pt-14 lg:px-8">
-        <div className="relative mx-auto max-w-2xl py-24">
-          <div className="absolute inset-x-0 -top-[4rem] -z-10 transform-gpu overflow-hidden blur-3xl md:-top-[10rem]">
-            <svg
-              className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-              viewBox="0 0 1155 678"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-                fillOpacity=".3"
-                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-              />
-              <defs>
-                <linearGradient
-                  id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-                  x1="1155.49"
-                  x2="-78.208"
-                  y1=".177"
-                  y2="474.645"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#9089FC" />
-                  <stop offset={1} stopColor="#FF80B5" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              AllProjects - WebHome11 - Projects & UI
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Great resources for Developers Ui & Projects
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-2">
-              <button
-                type="button"
-                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                Explore
-              </button>
-              <button
-                type="button"
-                className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                Projects
-              </button>
-              <button
-                type="button"
-                className="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                UI
-              </button>
-            </div>
-          </div>
+    <div className='flex justify-center items-center h-screen flex-col gap-3'>
+        <div className='flex justify-center items-start flex-col justify-center items-center gap-3'>
+        <h2 className='font-semibold text-4xl font-[Poppins] text-center ml-5 '>AllProjects - WebHome , <span><b>Great</b></span> resources for developers</h2>
+        <p className='text-center text-sm mr-5 ml-5'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat recusandae porro quidem minima sed id et maxime corrupti asperiores consectetur sapiente hic sint repellat eveniet, beatae quis cumque nemo quo.</p>
+        <div className='flex  justify-ceneter items-center '>
+            <Button variant='contained' sx={{
+                background : "black",
+               
+
+            }} >Explore.</Button>
+            
         </div>
-      </div>
+
+            <h1>h</h1>
+
+        </div>
+       
     </div>
   )
 }
 
+export default Home
--- AllProjects-Web-App/src/main.jsx
+++ AllProjects-Web-App/src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './components/Home/Home.jsx'
+
 import About from './components/About/About.jsx'
 
 import Contact from './components/Contact/Contact.jsx'
 import Apps from './components/Projects/Projects.jsx'
 import Projects from './components/Projects/Projects.jsx'
 import UI from './components/UI/UI.jsx'
+import Home from './components/Home/Home.jsx'
 const router = createBrowserRouter([
   {
     path : "/",
