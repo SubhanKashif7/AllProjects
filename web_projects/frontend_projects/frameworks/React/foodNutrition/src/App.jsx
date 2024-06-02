ca6b5659f71868e19de5aad9e7da70f741d79af2
= <=>
2024-06-02 12:38:27 +0500

    .

--- web_projects/frontend_projects/frameworks/React/foodNutrition/src/App.jsx
+++ web_projects/frontend_projects/frameworks/React/foodNutrition/src/App.jsx
@@ -3,7 +3,7 @@ import "./index.css"
 import Box from './components/Box'
 const App = () => {
   return (
-     <div className="w-full flex justify-center items-center bg-yellow-400">
+     <div className="w-full h-screen flex justify-center items-center bg-yellow-400">
       <Box/>
     </div>
   )
